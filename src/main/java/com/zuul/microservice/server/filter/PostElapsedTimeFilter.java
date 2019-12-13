acd2f9dc38225bc91d24aa3208db33b2f652b723
Adrian Ayarde <aayarde@overactive.com>
2019-12-13 10:14:21 -0300

    Use Hystrix, dynamic port, Zuul gateway and filters.

---
+++ src/main/java/com/zuul/microservice/server/filter/PostElapsedTimeFilter.java
@@ -0,0 +1,49 @@
+package com.zuul.microservice.server.filter;
+
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class PostElapsedTimeFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(PostElapsedTimeFilter.class);
+
+    @Override
+    public String filterType() {
+        return "post";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        log.info("Coming in post filter");
+
+        Long startTime = (Long) request.getAttribute("startTime");
+        Long finalTime = System.currentTimeMillis();
+        Long  elapsedTime = finalTime - startTime;
+
+        log.info(String.format("Elapsed Time in seconds %s seg.",elapsedTime.doubleValue()/100.00));
+        log.info(String.format("Elapsed Time in mileseconds %s ms.",elapsedTime));
+        return null;
+    }
+}
---
+++ src/test/java/com/zuul/microservice/server/ZuulMicroserviceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuul.microservice.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulMicroserviceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
